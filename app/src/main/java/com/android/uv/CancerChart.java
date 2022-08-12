d071fc2aa32357812d4cfab5f613ce35debeafae
zliu0188 <zliu0188@student.monash.edu>
2022-08-12 12:28:21 +1000

    Update Chart

--- app/src/main/java/com/android/uv/CancerChart.java
+++ app/src/main/java/com/android/uv/CancerChart.java
@@ -9,15 +9,24 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.Toast;
 
+import com.anychart.APIlib;
 import com.anychart.AnyChart;
 import com.anychart.AnyChartView;
 import com.anychart.chart.common.dataentry.DataEntry;
 import com.anychart.chart.common.dataentry.ValueDataEntry;
 import com.anychart.chart.common.listener.Event;
 import com.anychart.chart.common.listener.ListenersInterface;
+import com.anychart.charts.Cartesian;
 import com.anychart.charts.Pie;
+import com.anychart.core.cartesian.series.Line;
+import com.anychart.data.Mapping;
+import com.anychart.data.Set;
 import com.anychart.enums.Align;
+import com.anychart.enums.Anchor;
 import com.anychart.enums.LegendLayout;
+import com.anychart.enums.MarkerType;
+import com.anychart.enums.TooltipPositionMode;
+import com.anychart.graphics.vector.Stroke;
 
 
 import java.util.ArrayList;
@@ -28,12 +37,18 @@ public class CancerChart extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cancer_charts);
+        final AnyChartView anyChartView = findViewById(R.id.any_chart_view);
+
+        APIlib.getInstance().setActiveAnyChartView(anyChartView);
+        anyChartView.setProgressBar(findViewById(R.id.progress_bar));
+
+
         ActionBar actionBar = getSupportActionBar();
         actionBar.setHomeButtonEnabled(true);
         // add return icon.
         actionBar.setDisplayHomeAsUpEnabled(true);
 
-        Pie pie = AnyChart.pie();
+        final Pie pie = AnyChart.pie();
 
         pie.setOnClickListener(new ListenersInterface.OnClickListener(new String[]{"x", "value"}) {
             @Override
@@ -41,7 +56,6 @@ public class CancerChart extends AppCompatActivity {
                 Toast.makeText(CancerChart.this, event.getData().get("x") + ":" + event.getData().get("value"), Toast.LENGTH_SHORT).show();
             }
         });
-
 
         List<DataEntry> data = new ArrayList<>();
         data.add(new ValueDataEntry("Male",1703));
@@ -63,11 +77,91 @@ public class CancerChart extends AppCompatActivity {
                 .itemsLayout(LegendLayout.HORIZONTAL)
                 .align(Align.CENTER);
 
-
-        AnyChartView anyChartView = findViewById(R.id.any_chart_view);
-        anyChartView.setProgressBar(findViewById(R.id.progress_bar));
         anyChartView.setChart(pie);
+
+
+
+        //==========================================================================
+        AnyChartView anyChartView2 = findViewById(R.id.any_chart_view2);
+        APIlib.getInstance().setActiveAnyChartView(anyChartView2);
+
+        Cartesian cartesian = AnyChart.line();
+        cartesian.animation(true);
+        cartesian.padding(10d, 20d, 5d, 20d);
+        cartesian.crosshair().enabled(true);
+        cartesian.crosshair()
+                .yLabel(true)
+                .yStroke((Stroke) null, null, null, (String) null, (String) null);
+
+        cartesian.tooltip().positionMode(TooltipPositionMode.POINT);
+
+        cartesian.title("Melanoma of the skin");
+
+        cartesian.yAxis(0).title("Count number of cases each year");
+        cartesian.xAxis(0).labels().padding(5d, 5d, 5d, 5d);
+
+        List<DataEntry> seriesData = new ArrayList<>();
+        seriesData.add(new CustomDataEntry("2010", 204, 133));
+        seriesData.add(new CustomDataEntry("2011", 174, 143));
+        seriesData.add(new CustomDataEntry("2012", 194, 151));
+        seriesData.add(new CustomDataEntry("2013", 197, 138));
+        seriesData.add(new CustomDataEntry("2014", 180, 130));
+        seriesData.add(new CustomDataEntry("2015", 202, 126));
+        seriesData.add(new CustomDataEntry("2016", 209, 127));
+        seriesData.add(new CustomDataEntry("2017", 161, 132));
+        seriesData.add(new CustomDataEntry("2018", 183, 142));
+        seriesData.add(new CustomDataEntry("2019", 178, 129));
+        seriesData.add(new CustomDataEntry("2020", 171, 124));
+        seriesData.add(new CustomDataEntry("2021", 158, 116));
+        seriesData.add(new CustomDataEntry("2022", 153, 112));
+
+        Set set = Set.instantiate();
+        set.data(seriesData);
+        Mapping series1Mapping = set.mapAs("{ x: 'x', value: 'value' }");
+        Mapping series2Mapping = set.mapAs("{ x: 'x', value: 'value2' }");
+
+        Line series1 = cartesian.line(series1Mapping);
+        series1.name("Female");
+        series1.hovered().markers().enabled(true);
+        series1.hovered().markers()
+                .type(MarkerType.CIRCLE)
+                .size(4d);
+        series1.tooltip()
+                .position("left")
+                .anchor(Anchor.LEFT_CENTER)
+                .offsetX(5d)
+                .offsetY(5d);
+
+        Line series2 = cartesian.line(series2Mapping);
+        series2.name("Male");
+        series2.hovered().markers().enabled(true);
+        series2.hovered().markers()
+                .type(MarkerType.CIRCLE)
+                .size(4d);
+        series2.tooltip()
+                .position("right")
+                .anchor(Anchor.LEFT_CENTER)
+                .offsetX(5d)
+                .offsetY(5d);
+
+        cartesian.legend().enabled(true);
+        cartesian.legend().fontSize(13d);
+        cartesian.legend().padding(0d, 0d, 10d, 0d);
+
+
+
+
+        anyChartView2.setChart(cartesian);
     }
+
+    private class CustomDataEntry extends ValueDataEntry {
+        CustomDataEntry(String x, Number value, Number value2) {
+            super(x, value);
+            setValue("value2", value2);
+        }
+    }
+
+
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case android.R.id.home:
@@ -76,5 +170,11 @@ public class CancerChart extends AppCompatActivity {
                 return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+//        handler.removeCallbacks(runnable);
     }
 }
